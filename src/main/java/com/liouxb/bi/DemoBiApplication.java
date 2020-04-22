06f29487394e1c3fce5ee68fd62fdf62ea4f4c23
liouwb <liubin@cdiandian.com>
2020-04-22 10:44:21 +0800

    empty repository  commit

---
+++ src/main/java/com/liouxb/bi/DemoBiApplication.java
@@ -0,0 +1,13 @@
+package com.liouxb.bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liouxb/bi/DemoBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liouxb.bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
